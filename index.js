63d05d24b60bcf5059ceb9fcbdb1025301421f2e
Jordan  Haddadi <jordanhaddadi@gmail.com>
2018-05-15 20:49:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var name = "Joe"
 var height = "74"
-if (name = "Jordan") {console.log("The name is right")
+if (name = "Jordan") {console.log("The name is right")}
+else {console.log("crap, I expected the name to be Jordan")}
 
 
 // Don't worry about this
